4f659061ab8660d9c741be96acc9399298ac5ea9
ex0num <gabriel.lopez.gasal@gmail.com>
2025-04-18 13:09:27 -0300

    Fix: [Backend] - Arreglo de CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(helmet()); //--- Establece encabezados HTTP de respuesta que ayudan a mitigar ataques de tipo Cross-Site Scripting (XSS), de inyección de contenido (CSP), de secuestro de clics (Clickjacking) ------//
 
 app.use(cors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', "https://capitulito-frontend.vercel.app"],
+    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', "https://capitulito-frontend.vercel.app", "'https://capitulito-frontend.vercel.app'"],
     credentials: true,
     allowedHeaders: ['Authorization', 'Content-Type']
 })); //---- CORS es una medida de seguridad que restringe cómo los recursos en una página web pueden ser solicitados desde otro dominio diferente al dominio de origen. Es útil cuando tu servidor Express necesita permitir solicitudes de recursos desde orígenes cruzados ---//
@@ -32,7 +32,9 @@ app.get('/', async (req, res) => {res.json({name: app.get('pkg').name, autor: ap
 app.use('/api/movies', peliculasRoutes);
 
 // --- Port defined --- //
-const PORT = process.env.PORT || 3101;
-app.listen(PORT);
+// const PORT = process.env.PORT || 3101;
+// app.listen(PORT);
+
+module.exports = app;
 
 console.log('----- Server listening on port -----', PORT);
